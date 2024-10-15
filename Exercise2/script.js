72a0118fe7a103d66f1ab5a6b41053c784284a49
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-10-15 13:09:25 +0200

    Ex2 | step 3.

--- Exercise2/script.js
+++ Exercise2/script.js
@@ -1,2 +1,42 @@
 "use strict"
 let todoList = [];
+
+let initList = function() {
+    todoList.push(
+        {
+            title: "Learn JS",
+            description: "Create a demo application for my TODO's",
+            place: "445",
+            category: '',
+            dueDate: new Date(2024,10,16)
+        },
+        {
+            title: "Lecture test",
+            description: "Quick test from the first three lectures",
+            place: "F6",
+            category: '',
+            dueDate: new Date(2024,10,17)
+        }
+    );
+}
+
+initList();
+
+let updateTodoList = function() {
+    let todoListDiv =
+        document.getElementById("todoListView");
+
+    while (todoListDiv.firstChild) {
+        todoListDiv.removeChild(todoListDiv.firstChild);
+    }
+
+    for (let todo in todoList) {
+        let newElement = document.createElement("div");
+        let newContent = document.createTextNode(
+            todoList[todo].title + " " + todoList[todo].description);
+        newElement.appendChild(newContent);
+        todoListDiv.appendChild(newElement);
+    }
+}
+
+setInterval(updateTodoList, 1000);
