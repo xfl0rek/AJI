f2c5ae62ab59909d44e1831836d44297dd86da03
Wojciech Florczak <florczak.wojciechv@gmail.com>
2025-01-08 22:31:09 +0100

    change order status

--- Exercise3/backend/index.js
+++ Exercise3/backend/index.js
@@ -17,7 +17,7 @@ const cors = require('cors');
 
 const corsOptions = {
     origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
